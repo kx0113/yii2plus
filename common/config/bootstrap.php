2dfb796c1e5c8ff2e2a38ce0a9eaaa97036b740e
xsk <879461667@qq.com>
2017-05-07 20:39:16 +0800

    edit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@webq', dirname(dirname(__DIR__)) . '/webq');
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@webone', dirname(dirname(__DIR__)) . '/webone');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
---
+++ common/config/main-local.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=rm-2ze5j61837k299wd3o.mysql.rds.aliyuncs.com;dbname=yii2plus',
+            'username' => 'root',
+            'password' => 'xsk1995@@',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        /*'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'itemTable' => 'auth_item',
+            'assignmentTable' => 'auth_assignment',
+            'itemChildTable' => 'auth_item_child',
+        ],*/
+    ],
+];
