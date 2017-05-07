f2de73198ec257736885f042d9cdf52954a5bb2d
xsk <879461667@qq.com>
2017-05-07 23:29:50 +0800

    122

--- common/config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@webq', dirname(dirname(__DIR__)) . '/webq');
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@webone', dirname(dirname(__DIR__)) . '/webone');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('@data', dirname(dirname(__DIR__)) . '/data');
